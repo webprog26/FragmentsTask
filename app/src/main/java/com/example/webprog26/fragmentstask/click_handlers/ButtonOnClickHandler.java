ed2fd5ac65ea910709e53aba62aa2cbf06002b69
Volodymyr Shalashenko <webprog26@gmail.com>
2016-12-01 11:15:02 +0200

    Useless code in AddArticle button onClick handler removed

--- app/src/main/java/com/example/webprog26/fragmentstask/click_handlers/ButtonOnClickHandler.java
+++ app/src/main/java/com/example/webprog26/fragmentstask/click_handlers/ButtonOnClickHandler.java
@@ -31,22 +31,9 @@ public class ButtonOnClickHandler implements View.OnClickListener {
         switch (view.getId()){
             case R.id.btnAddArticle:
                 //Calling EditorActivity from MainActivity to open FragmentEditor
-//                if(mActivity.findViewById(R.id.fragmentEditor) == null){
                     Intent intent = new Intent(mActivity, EditorActivity.class);
                     intent.putExtra(FragmentEditor.ARTICLE_ID, FragmentEditor.NEW_ARTICLE);
                     mActivity.startActivity(intent);
-//                } else {
-//                    FragmentManager fragmentManager = ((AppCompatActivity)mActivity).getSupportFragmentManager();
-//                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                    Fragment fragmentEditor = fragmentManager.findFragmentById(R.id.fragmentEditor);
-//                    Fragment newFragmentEditor = FragmentEditor.newInstance(FragmentEditor.NEW_ARTICLE);
-//                    if(fragmentEditor != null){
-//                        fragmentTransaction.remove(fragmentEditor);
-//                    }
-//
-//                    fragmentTransaction.add(R.id.fragmentEditor, newFragmentEditor);
-//                    fragmentTransaction.commit();
-//                }
                 break;
             case R.id.btnCancel:
                 //Finishing EditorActivity. Returning to Main Activity
